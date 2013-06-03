0c996e33c5f8d2454ca6cbdd23f69d59685be375
Philip Brown <phil@ipbrown.com>
2013-06-03 10:56:51 +0100

    Add bootstrap shell script

---
+++ bootstrap.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+# Update the box
+# --------------
+# Downloads the package lists from the repositories
+# and "updates" them to get information on the newest
+# versions of packages and their dependencies
+apt-get update
+
+# Install Vim
+apt-get install -y vim
+
+# Apache
+# ------
+# Install
+apt-get install -y apache2
+# Remove /var/www default
+rm -rf /var/www
+# Symlink /vagrant to /var/www
+ln -fs /vagrant /var/www
+# Add ServerName to httpd.conf
+echo "ServerName localhost" > /etc/apache2/httpd.conf
+# Setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+  DocumentRoot "/vagrant/public"
+  ServerName localhost
+  <Directory "/vagrant/public">
+    AllowOverride All
+  </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-enabled/000-default
+# Enable mod_rewrite
+a2enmod rewrite
+# Restart apache
+service apache2 restart
+
+# PHP 5.4
+# -------
+apt-get install -y libapache2-mod-php5
+# Add add-apt-repository binary
+apt-get install -y python-software-properties
+# Install PHP 5.4
+add-apt-repository ppa:ondrej/php5
+# Update
+apt-get update
+
+# PHP stuff
+# ---------
+# Command-Line Interpreter
+apt-get install -y php5-cli
+# MySQL database connections directly from PHP
+apt-get install -y php5-mysql
+# cURL is a library for getting files from FTP, GOPHER, HTTP server
+apt-get install -y php5-curl
+# Module for MCrypt functions in PHP
+apt-get install -y php5-mcrypt
+
+# cURL
+# ----
+apt-get install -y curl
+
+# Mysql
+# -----
+# Ignore the post install questions
+export DEBIAN_FRONTEND=noninteractive
+# Install MySQL quietly
+apt-get -q -y install mysql-server-5.5
+
+# Git
+# ---
+apt-get install git-core
+
+# Install Composer
+# ----------------
+curl -s https://getcomposer.org/installer | php
+# Make Composer available globally
+mv composer.phar /usr/local/bin/composer
+
+# Laravel stuff
+# -------------
+# Load Composer packages
+cd /var/www
+composer install --dev
+# Set up the database
+echo "CREATE DATABASE IF NOT EXISTS your_database" | mysql
+echo "CREATE USER 'your_username'@'localhost' IDENTIFIED BY 'your_password'" | mysql
+echo "GRANT ALL PRIVILEGES ON your_database.* TO 'your_username'@'localhost' IDENTIFIED BY 'your_password'" | mysql
+# Set up the database
+php artisan migrate
